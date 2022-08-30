a731f30be19884d00b9243244e1496dda399f0d9
sathya.gimenes <sathya.gimenes@gmail.com>
2022-08-29 22:02:59 -0300

    Codigo atualizado com decorators, StatusCode e validações básicas

--- WEBAPI.Aula01/Cadastro.cs
+++ WEBAPI.Aula01/Cadastro.cs
@@ -1,18 +1,20 @@
-﻿namespace WEBAPI.Aula01
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEBAPI.Aula01
 {
     public class Cadastro
     {
+        [Required]
         public string? Nome { get; set; }
 
-        public long Cpf { get; set; }
+        [MinLength(11)]
+        [MaxLength(11)]
+        [Required]
+        public string Cpf { get; set; }
 
+        [Required(ErrorMessage = "Data é obrigatória")]
         public DateTime DataNascimento { get; set; }
+        
         public int Idade => DateTime.Now.Year - DataNascimento.Year;
-
-        public override string ToString()
-        {
-            return $"Nome: {Nome}\nCpf: {Cpf}\nData de nascimento {DataNascimento}" +
-                $"\nIdade: {Idade}";
-        }
     }
 }
--- WEBAPI.Aula01/Controllers/CadastroController.cs
+++ WEBAPI.Aula01/Controllers/CadastroController.cs
@@ -4,15 +4,12 @@ using System.Linq;
 
 namespace WEBAPI.Aula01.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
+    [Consumes("application/json")] // Determina o que será consumido
+    [Produces("application/json")] // Determina o que será devolvido
     public class CadastroController : ControllerBase
     {
-        //Enunciado
-        // Construa um cadastro completo (CRUD) de clientes.
-        // Neste cadastro, o cliente deve possuir cpf, nome,
-        // data de nascimento e idade.
-
         private static readonly string[] Nomes = new[]
         {
             "Harry Potter", "Hermione Granger", "Ronald Weasley", "Luna Lovegood", "Neville Longbottom"
@@ -25,57 +22,76 @@ namespace WEBAPI.Aula01.Controllers
             _logger = logger;
             cadastrosCliente = Enumerable.Range(0, 5).Select(num => new Cadastro
             {
-                DataNascimento = new DateTime((1960+ num), 01, 05),
+                DataNascimento = new DateTime((1960 + num), 01, 05),
                 Nome = Nomes[num],
-                Cpf = 12345678900 + (long)num
+                Cpf = Convert.ToString(12345678900 + num)
             })
             .ToList();
         }
 
         // GET api/<CadastroController>
-        [HttpGet]
-        public List<Cadastro> VerTudo()
+        [HttpGet("/cliente/consulta")] // Especifica a rota
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Cadastro>> VerTudo()
         {
-            return cadastrosCliente;
+            return Ok(cadastrosCliente);
         }
 
         // GET api/<CadastroController>
-        [HttpGet("{cpf}")]
-        public Cadastro? Consulta(long cpf)
+        [HttpGet("/cliente/{cpf}/consulta")] // Especifica a rota. O parametro vira path
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Cadastro> Consulta(string cpf)
         {
             var cadastro = cadastrosCliente.Find(cadastrosCliente => cadastrosCliente.Cpf == cpf);
-            if (cadastro != null)
-                return cadastro;
-            else
-                return null;
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+            return Ok(cadastro);
         }
 
         //POST api/<CadastroController>
-        [HttpPost]
-        public Cadastro Inserir(Cadastro clienteNovo)
+        [HttpPost("/cliente/inserção")] // Sem o path determinado, o parametro é query
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Cadastro> Inserir(Cadastro clienteNovo)
         {
             cadastrosCliente.Add(clienteNovo);
-            return clienteNovo;
+            return CreatedAtAction(nameof(Inserir), clienteNovo);
         }
 
         // PUT api/<CadastroController>
-        [HttpPut]
-        public Cadastro? Atualizar(long cpf, Cadastro novoCadastro)
+        [HttpPut("/cliente/{cpf}/atualização")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Cadastro> Atualizar(string cpf, Cadastro novoCadastro)
         {
             var cadastro = cadastrosCliente.Find(cadastrosCliente => cadastrosCliente.Cpf == cpf);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
             cadastro.Nome = novoCadastro.Nome;
             cadastro.Cpf = novoCadastro.Cpf;
             cadastro.DataNascimento = novoCadastro.DataNascimento;
-            return cadastrosCliente.Find(cadastrosCliente => cadastrosCliente.Cpf == novoCadastro.Cpf);
+            return Ok(cadastrosCliente.Find(cadastrosCliente => cadastrosCliente.Cpf == novoCadastro.Cpf));
         }
 
         // DELETE api/<CadastroController>
-        [HttpDelete]
-        public List<Cadastro> Deletar(long cpf)
+        [HttpDelete("/cliente/{cpf}/remoção")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Deletar(string cpf)
         {
             var cadastro = cadastrosCliente.Find(cadastrosCliente => cadastrosCliente.Cpf == cpf);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
             cadastrosCliente.Remove(cadastro);
-            return cadastrosCliente;
+            return NoContent();
         }
     }
 }
